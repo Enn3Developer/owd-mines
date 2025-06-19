f78b6dd6b279dcb8ca2a4cd04df8b17d7b41c5cb
Enn3Developer <arturpeshko39@gmail.com>
2025-06-19 16:58:27 +0200

    updating sizes for game window

--- runtime/components/Window/WindowTemplate.vue
+++ runtime/components/Window/WindowTemplate.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-// do magic
+const props = defineProps<{
+  window: WindowController
+}>()
 </script>
 
 <template>
-  <Window :content="{ padded: true }">
-    <iframe src="https://mines.enn3.ovh/"></iframe>
+  <Window v-bind="$props">
+    <iframe src="https://mines.enn3.ovh/" :width="props.window.state.sizes.width" :height="props.window.state.sizes.height"></iframe>
   </Window>
 </template>
 
