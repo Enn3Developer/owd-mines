b41e78ee175306966640e112cc8a7051874e9390
Enn3Developer <arturpeshko39@gmail.com>
2025-06-19 17:07:05 +0200

    trying to fix bug

--- runtime/components/Window/WindowTemplate.vue
+++ runtime/components/Window/WindowTemplate.vue
@@ -5,8 +5,8 @@ const props = defineProps<{
 </script>
 
 <template>
-  <Window v-bind="$props" v-on:load="console.log(props.window)">
-    <iframe src="https://mines.enn3.ovh/" :width="640" :height="480"></iframe>
+  <Window v-bind="$props">
+    <iframe src="https://mines.enn3.ovh/" :width="640" :height="480" v-on:load="console.log(props.window)"></iframe>
   </Window>
 </template>
 
